465c0a8152a47f41ff2605ce11778f13688c8a25
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2026-08-10 17:52:50 +0200

    core lib

--- apps/adoption_agency.ui/src/app/app.routes.ts
+++ apps/adoption_agency.ui/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('@adoption-agency.ui/core').then((m) => m.coreRoutes),
+  },
+];
---
+++ libs/core/src/lib/lib.routes.ts
@@ -0,0 +1,4 @@
+import { Route } from '@angular/router';
+import { Core } from './core/core';
+
+export const coreRoutes: Route[] = [{ path: '', component: Core }];
