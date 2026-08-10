18b0361d277526371cbb7e341bf2831e24c04410
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2026-08-10 17:41:46 +0200

    install api interfaces

--- apps/adoption_agency.ui/src/app/app.config.ts
+++ apps/adoption_agency.ui/src/app/app.config.ts
@@ -1,10 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { BASE_PATH as PetsServiceBasePath } from '@adoption-agency/pets-service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(appRoutes)
+    provideRouter(appRoutes),
+    {
+      provide: PetsServiceBasePath,
+      useValue: 'http://localhost:8080'
+    }
   ]
 };
