fe49d8aa4117bc66bbb98a0079c84bd7365772c1
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2026-08-13 15:40:43 +0200

    common

--- libs/core/src/lib/core/core.ts
+++ libs/core/src/lib/core/core.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'lib-core',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './core.html',
   styleUrl: './core.css',
 })
