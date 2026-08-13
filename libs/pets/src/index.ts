d9d0acb95891c71d759ad324041c563fe60a480d
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2026-08-13 08:32:31 +0200

    remove missing import

--- libs/pets/src/index.ts
+++ libs/pets/src/index.ts
@@ -4,7 +4,6 @@ import * as PetsFeature from './lib/state/pets.reducer';
 
 import * as PetsSelectors from './lib/state/pets.selectors';
 
-export * from './lib/state/pets.models';
 
 export { PetsActions, PetsFeature, PetsSelectors };
 export * from './lib/lib.routes';
